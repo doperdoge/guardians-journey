c349f9d6db86140370be060a3c5552f046ba4eff
Conner Hsieh <67612639+doperdoge@users.noreply.github.com>
2026-08-30 21:31:55 -0700

    uncommented manifest

--- app/api/user-data/route.ts
+++ app/api/user-data/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest){
   const membershipId = searchParams.get('membershipID');
 
   // Destiny2.GetHistoricalStatsForAccount API Call
-  const historical_stats = await fetch(`https://www.bungie.net/Platform/Destiny2/${membershipType}/Account/${membershipId}/Stats/`,{
+  const historical_stats = await fetch(`https://www.bungie.net/Platform/Destiny2/${membershipType}/Account/${membershipId}/Stats/?definition=true`,{
     method: "GET",
     headers,
   });
@@ -30,6 +30,14 @@ export async function GET(request: NextRequest){
   const profile_data = await historical_stats.json();
   const user_characters  = profile_data.Response.characters;
   const user_data = [];
+
+  // Destiny2.GetDestinyManifest
+  const manifest = await fetch(`https://www.bungie.net/Platform/Destiny2/Manifest/`,{
+    method: "GET",
+    headers, 
+  });
+
+  const manifestData = await manifest.json();
 
   for(let i = 0; i < user_characters.length; i++){
     if(user_characters[i].deleted == true ) continue;
@@ -58,18 +66,10 @@ export async function GET(request: NextRequest){
     user_data.push({
       activityHistory: actHistData,
       aggregateStats: aggStatsData,
-      weaponsStats: wepStatsData
+      weaponsStats: wepStatsData,
+      manifests: manifestData
     })
   }
-
-  // Destiny2.GetDestinyManifest
-  // const accStats = await fetch(`https://www.bungie.net/Platform/Destiny2/Manifest/`,{
-  //   method: "GET",
-  //   headers: {
-  //       "X-API-Key": apiKey
-  //   }, 
-  // });
-
 
   return new Response(JSON.stringify(user_data[0]), {
     status: 200,
