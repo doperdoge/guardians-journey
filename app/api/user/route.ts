0b0ed8b2e56c82a8b748383a1a4898ca49d79e0e
Conner Hsieh <67612639+doperdoge@users.noreply.github.com>
2026-09-02 01:30:57 -0700

    api verification

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,12 +4,19 @@ require('dotenv').config()
 
 const apiKey = process.env.APIKEY
 
+
+
 export async function GET(request: NextRequest){
+  // checks to see if there is an api key
+  if (!apiKey){
+      throw new Error("APIKEY not configured");
+  }
+
     // Parse the request body
   const searchParams = request.nextUrl.searchParams;
   const res = await fetch("https://www.bungie.net/Platform/User/Search/GlobalName/0/",{
     method: "POST",
-    headers: {
+    headers:{
         "X-API-Key": apiKey
     },
     body: JSON.stringify({displayNamePrefix: searchParams.get('username')}),
